cccaf5386ef0f6a0c284bb73d4f5cad33ece02db
Justin Reich <justinreich.dev@gmail.com>
2018-04-30 11:12:15 -0500

    Add Devtools and start creating seeder

---
+++ src/main/java/com/codeup/fadlister/seeders/DatabaseSeeder.java
@@ -0,0 +1,13 @@
+package com.codeup.fadlister.seeders;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
